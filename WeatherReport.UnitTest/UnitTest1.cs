560953a5041f375dd90cd78b953fa7ba166022a5
goutamroy84 <124436860+goutamroy84@users.noreply.github.com>
2023-02-04 06:15:26 -0600

    Unit Test updated

--- WeatherReport.UnitTest/UnitTest1.cs
+++ WeatherReport.UnitTest/UnitTest1.cs
@@ -1,8 +1,10 @@
-using System.ComponentModel;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WeatherReport;
+using Newtonsoft.Json;
 using WeatherReport.Model;
 
 namespace WeatherReport.UnitTest
@@ -26,6 +28,25 @@ namespace WeatherReport.UnitTest
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"CityInformation\IndiaCity.json");
             bool isequal = File.Exists(path);
             Assert.IsTrue(isequal);
-        }     
+        } 
+        
+        [TestMethod]
+        public void CityPresentInJson()
+        {
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"CityInformation\IndiaCity.json");
+            string _countryJson = File.ReadAllText(path);
+            var _country = JsonConvert.DeserializeObject<List<IndiaCity>>(_countryJson);
+            int i = 0;
+            foreach (var item in _country)
+            {
+                i++;
+                var result = from s in _country
+                             where s.city == item.city
+                             select s;
+            }
+            Assert.IsTrue(i > 0);
+
+        }
+        
     }
 }
